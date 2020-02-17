4a1ed1b01ec37847f10ebe2cc73f7abaa0be2923
chips.wang <chips.wang@oneplus.com>
2020-02-17 17:19:08 +0800

    测试

--- app/src/main/java/com/jixuan/wavedemo/CircleView.java
+++ app/src/main/java/com/jixuan/wavedemo/CircleView.java
@@ -20,6 +20,10 @@ import android.view.Display;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
+
+
+
+
 import androidx.annotation.Nullable;
 
 import java.util.Timer;
